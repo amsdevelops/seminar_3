fcf5fff970aa2b8054b104f1f6171e2fced7d224
amsdevelops <amsdevelops@gmail.com>
2023-07-22 22:59:07 +0300

    task 3 completed

--- app/src/main/java/com/example/seminar_3/presentation/view/MainActivity.kt
+++ app/src/main/java/com/example/seminar_3/presentation/view/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.seminar_3.presentation.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -8,6 +10,7 @@ import com.example.seminar_3.databinding.ActivityMainBinding
 import com.example.seminar_3.presentation.viewmodel.MainActivityViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -18,24 +21,35 @@ class MainActivity : AppCompatActivity() {
         ViewModelProvider(this)[MainActivityViewModel::class.java]
     }
     private val scope = CoroutineScope(Dispatchers.IO)
+    private var searchJob: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.search.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
 
-        binding.button.setOnClickListener {
-            scope.launch {
-                val result = viewModel.search(binding.search.text.toString())
-                withContext(Dispatchers.Main) {
-                    if (result.isEmpty()) {
-                        Toast.makeText(this@MainActivity, "Ничего не найдено", Toast.LENGTH_SHORT).show()
-                    } else {
-                        binding.result.text = result.toString()
+            override fun onTextChanged(p0: CharSequence, p1: Int, p2: Int, p3: Int) {
+                searchJob?.cancel()
+                searchJob = scope.launch {
+                    val result = viewModel.search(binding.search.text.toString())
+                    withContext(Dispatchers.Main) {
+                        if (result.isEmpty()) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Ничего не найдено",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            binding.result.text = result.toString()
+                        }
                     }
                 }
             }
-        }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
     }
 
     override fun onDestroy() {
